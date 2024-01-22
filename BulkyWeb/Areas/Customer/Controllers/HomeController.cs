fae9c3089de7dc0d517a40a707c859c59599279b
Sachith Thilina <sachithilina@hotmail.com>
2024-01-23 03:18:34 +0530

    Details Action Added. Only Action Not with UI

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -22,6 +22,12 @@ namespace BulkyWeb.Areas.Customer.Controllers
             return View(productList);
         }
 
+        public IActionResult Details(int productId)   //Showing all information when click the 'Details' Btn 
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+            return View(product);
+        }
+
         public IActionResult Privacy()
         {
             return View();
